258cecbcdc2b993ec4f84a04574276ea280aedd8
lishaopeng <lishaopeng@bjshenpu.com>
2023-12-14 15:01:19 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
-const {PORT} = require('./config/config.dotenv')
+// const {PORT} = require('./config/config.dotenv')
+const port = process.env.PORT || 80;
+
 const app = require('./app/index')//导入app文件
-app.listen(PORT,()=>{
-    console.log(`启动的服务的端口是http://localhost:${PORT}`)
+app.listen(port,()=>{
+    console.log(`启动的服务的端口是http://localhost:${port}`)
 })
